9fd2610b2de8a97783a154b7a7d985e85c6794b6
Rich <ricardoalexis.huerta@gmail.com>
2025-11-06 02:53:20 -0600

    build 6 nov again again

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2508514445"
+    serviceWorkerVersion: "2946611884"
   }
 });
